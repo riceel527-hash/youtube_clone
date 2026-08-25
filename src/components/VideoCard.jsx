bf63ff1d65faf7dcacd88823c4ca508f957db745
riceel527-hash <riceel527@gmail.com>
2026-08-25 14:11:17 -0400

    added css

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle, TextSnippetRounded } from '@mui/icons-material';
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
+
+
+const VideoCard = ({ video: { id: { videoId },snippet } }) => {
+    console.log(videoId, snippet);
+
+  return (
+    <div>VideoCard</div>
+  )
+}
+
+export default VideoCard
