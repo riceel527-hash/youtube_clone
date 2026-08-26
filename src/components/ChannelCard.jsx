c43e8145bae486366a28133bd11a2541d9741626
riceel527-hash <riceel527@gmail.com>
2026-08-26 14:33:53 -0400

    added css

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
-const ChannelCard = () => {
-  return (
-    <div>ChannelCard</div>
+import { demoProfilePicture } from '../utils/constants';
+
+
+const ChannelCard = ({channelDetail}) => (
+    <Box>
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+      }} 
+      >
+
+      <Link to={ `/channel/${channelDetail?.id?.channelId}`} >
+       <CardContent sx={{ display: 'flex', flexDirection:
+       'column',justifyContent: 'center', textAlign: 'center', color: '#fff'}}>
+
+      </CardContent>
+      
+      </Link> 
+
+    </Box>
   )
-}
 
 export default ChannelCard
