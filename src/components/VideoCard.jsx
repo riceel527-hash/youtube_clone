72affefec070b0c9971b1409f7abe75a2b744c7d
riceel527-hash <riceel527@gmail.com>
2026-08-26 09:59:50 -0400

    added css

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,10 +6,28 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 
 
 const VideoCard = ({ video: { id: { videoId },snippet } }) => {
-    console.log(videoId, snippet);
+    
 
   return (
-    <div>VideoCard</div>
+    <Card>
+        <Link to={videoId ? `/video/${videoId}`:demoVideoUrl} >
+         <CardMedia
+         image= {snippet?.thumbnails?.high?.url}
+         alt={snippet?.title}
+         sx={{ width: 358, height: 180 }}
+         />
+        </Link>
+        <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }} />
+        <Link to={videoId ? `/video/${videoId}`:demoVideoUrl} >
+        <Typography variant="subtitle1"fontweight="bold" color="#fff">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+         </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}`: demoChannelUrl}>
+        <Typography variant="subtitle2"fontweight="bold" color="#fff">
+            {snippet?.channelTitle || demoChannelTitle}
+        </Typography>
+    </Card>
   )
 }
 
