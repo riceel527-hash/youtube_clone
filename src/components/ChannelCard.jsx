b1ed36babd4687aafab1e5e6f83370077ab305a4
riceel527-hash <riceel527@gmail.com>
2026-08-27 15:30:23 -0400

    added css

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -6,10 +6,16 @@ import { demoProfilePicture } from '../utils/constants';
 
 
 const ChannelCard = ({channelDetail}) => (
-    <Box>
+    <Box
       sx={{
         boxShadow: 'none',
         borderRadius: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: { xs: '356px', md: '320px'},
+        height: '326px',
+        margin: 'auto',
       }} 
     >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
@@ -17,7 +23,16 @@ const ChannelCard = ({channelDetail}) => (
        'column',justifyContent: 'center', textAlign: 'center', color: '#fff'}}>
         <CardMedia
          image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture }
-         alt={channelDetail?.snippet?.title}sx={{ borderRadius: '50%', height: '180px', width: '180px'}} />
+         alt={channelDetail?.snippet?.title}sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3'}} />
+      <Typography variant="h6">
+        {channelDetail?.snippet?.title}<CheckCircle sx ={{ fontSize: 14, color: 'gray', ml: '5px'}} />
+      </Typography>
+      {channelDetail?.statistics?.subsciberCount && (
+        <Typography>
+            {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+        </Typography>
+      )}
+
       </CardContent>
       
       </Link> 
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,6 +1,11 @@
- import React from 'react'
+ import { useState, useEffect } from 'react';
+ import { useParams } from 'react-router-dom';
+ import { Box } from '@mui/material';
+
+ import {Videos, ChannelCard } from './';
  
  const ChannelDetail = () => {
+  const { id } = useParams();
    return (
      <div>ChannelDetail</div>
    )
