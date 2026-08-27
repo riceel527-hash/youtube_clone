24c06e2fc4827772c6d8a94079c2b02419ae858a
riceel527-hash <riceel527@gmail.com>
2026-08-27 12:48:53 -0400

    added css

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -12,13 +12,12 @@ const ChannelCard = ({channelDetail}) => (
         borderRadius: '20px',
       }} 
     >
-
-      <Link to={ `/channel/${channelDetail?.id?.channelId}`} >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
        <CardContent sx={{ display: 'flex', flexDirection:
        'column',justifyContent: 'center', textAlign: 'center', color: '#fff'}}>
         <CardMedia
-         image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture }/>
-         alt={channelDetail?.snippet?.title}sx={{ borderRadius: '50%', height: '180px', width: '180px'}}
+         image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture }
+         alt={channelDetail?.snippet?.title}sx={{ borderRadius: '50%', height: '180px', width: '180px'}} />
       </CardContent>
       
       </Link> 
