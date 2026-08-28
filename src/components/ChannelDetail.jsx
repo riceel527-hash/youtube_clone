2a2e740773dbd7215e1c550b29e9d03e37a1f5a1
riceel527-hash <riceel527@gmail.com>
2026-08-28 17:28:14 -0400

    added css

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,41 +1,43 @@
- import { useState, useEffect } from 'react';
- import { useParams } from 'react-router-dom';
- import { Box } from '@mui/material';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
 
- import {Videos, ChannelCard } from './';
- import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
- 
- const ChannelDetail = () => {
-  const [channelDetail, setChannelDetail] = useState(null)
-  const [videos, setVideos] = useState([])
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
 
   const { id } = useParams();
-
-  console.log(channelDetail, videos)
-
   useEffect(() => {
-    fetchFromAPI(`channels?part="snippet&${id}`)
-    .then((data) => setChannelDetail(data?.items[0]));
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => {
+      console.log("Data coming back from API: ", data);
+      setChannelDetail(data?.items[0]);
+    });
 
-    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
-    .then((data) => setVideos(data?.items));
-  },[id])
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items),
+    );
+  }, [id]);
 
-   return (
-     <Box minHeight="95vh">
+  return (
+    <Box minHeight="95vh">
       <Box>
-        <div style={{
-         background: 'linear-gradient(90deg, rgba(0, 238, 247, 1) 0%, rgba(206, 3, 184, 1)100%, rgba(0, 212, 255, 1) 100%)' ,
-       zIndex: 10,
-       height: '300px',
-      }}
-      />
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(0, 238, 247, 1) 0%, rgba(206, 3, 184, 1)100%, rgba(0, 212, 255, 1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
         <ChannelCard channelDetail={channelDetail} />
-        </Box>
-     </Box>
-   )
- }
- 
- export default ChannelDetail
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
+
 
